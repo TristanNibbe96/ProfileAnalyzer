4ff7bb4450681c9c12fcf7e0424911534590fee4
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-11 23:42:41 -0700

    added common word class

--- src/ProfileAnalyzer/CommonWord.java
+++ src/ProfileAnalyzer/CommonWord.java
@@ -1,4 +1,18 @@
 package ProfileAnalyzer;
 
-public class CommonWord {
+public class CommonWord implements Comparable{
+    public int count;
+    public String word;
+
+
+    @Override
+    public int compareTo(Object o) {
+        if(!(o instanceof CommonWord other)){
+            System.out.println("ERROR: Attempting to compare non instance of CommonWord to CommonWord!");
+            return 0;
+        }
+
+        return other.count - this.count;
+    }
 }
+
