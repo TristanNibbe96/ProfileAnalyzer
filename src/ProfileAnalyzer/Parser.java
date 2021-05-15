c3dcf65ae5dd36c811a3babf1dfebc883bfe8fed
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-14 18:44:24 -0700

    made constructor for parser

--- src/ProfileAnalyzer/Parser.java
+++ src/ProfileAnalyzer/Parser.java
@@ -1,10 +1,27 @@
 package ProfileAnalyzer;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class Parser {
 
+    List<String> irrelevantWords;
+    String directory;
+
     public Parser(String directory){
+        this.directory = directory;
+
+        irrelevantWords = new ArrayList<>();
+
+        try{setUpIrrelevantWords();}
+        catch (FileNotFoundException e){
+            System.out.println("No File with irrelevant wprds fpund");
+        }
+    }
+
+    public void setUpIrrelevantWords() throws FileNotFoundException{
 
     }
 
