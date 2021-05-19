f0e89307c9f6abd5353ab905f4cc9104c1024050
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-18 19:04:47 -0700

    started adding to analyzer

--- src/ProfileAnalyzer/Analyzer.java
+++ src/ProfileAnalyzer/Analyzer.java
@@ -1,14 +1,28 @@
 package ProfileAnalyzer;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Analyzer {
+    private LinkedList<CommonWord> commonWords;
 
-    public void printCommonWords(int limit){
+    public void analyzeProfile(HashSet<String> profile){
+        if (commonWords == null){
+            setupNewCommonWords(profile);
+        }else {
+            processNewProfile(profile);
+        }
+    }
+
+    private void setupNewCommonWords(HashSet<String> profile){
 
     }
 
-    public void analyzeProfile(HashSet<String> profile){
+    private  void processNewProfile(HashSet<String> profile){
+
+    }
+
+    public void printCommonWords(int limit){
 
     }
 }
