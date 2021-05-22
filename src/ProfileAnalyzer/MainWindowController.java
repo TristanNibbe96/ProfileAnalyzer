2da1aa8cf03e1fc60d861964db59d8f951f9a553
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-21 19:14:28 -0700

    switched to MainWindowController to use as driver of program

--- src/ProfileAnalyzer/MainWindowController.java
+++ src/ProfileAnalyzer/MainWindowController.java
@@ -1,4 +1,41 @@
 package ProfileAnalyzer;
 
-public class MainWindowController {
+import ProfileAnalyzer.Analyzer;
+import ProfileAnalyzer.Parser;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.*;
+
+public class MainWindowController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Profile Analyzer");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        String directory = "C:\\Users\\Tristan_Nibbe\\Downloads";
+
+        Parser parser = new Parser(directory);
+        Analyzer analyzer = new Analyzer();
+
+        File directoryPath = new File(directory + "\\Reference_Profiles");
+        String contents[] = directoryPath.list();
+
+        for(String file: contents) {
+            HashSet<String> parsedProfile = parser.parseProfile(file);
+
+            analyzer.analyzeProfile(parser.parseProfile(file));
+        }
+
+        analyzer.printCommonWords(3);
+    }
 }
