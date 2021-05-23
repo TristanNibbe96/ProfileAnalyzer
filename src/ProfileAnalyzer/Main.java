e64156e568109fb75ca74ed39826a77f9d6d90d3
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-22 19:25:38 -0700

    refactored a bunch of files

--- src/ProfileAnalyzer/Main.java
+++ src/ProfileAnalyzer/Main.java
@@ -6,20 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.util.HashSet;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Profile Analyzer");
+        primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-    public static void main(String[] args) {launch(args);
-    }
 
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/ProfileAnalyzer/MainWindowController.java
+++ src/ProfileAnalyzer/MainWindowController.java
@@ -1,18 +1,4 @@
 package ProfileAnalyzer;
 
-import ProfileAnalyzer.Analyzer;
-import ProfileAnalyzer.Parser;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.File;
-import java.util.*;
-
-public class MainWindowController  {
-
-
-
+public class MainWindowController {
 }
