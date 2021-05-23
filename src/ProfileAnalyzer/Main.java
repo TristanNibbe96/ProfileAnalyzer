30f226cce34c6b80d500396f9856e1c775104d66
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-22 19:21:44 -0700

    recreated old placeholder file

---
+++ src/ProfileAnalyzer/Main.java
@@ -0,0 +1,25 @@
+package ProfileAnalyzer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.HashSet;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("Profile Analyzer");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {launch(args);
+    }
+
+}
--- src/ProfileAnalyzer/MainWindowController.java
+++ src/ProfileAnalyzer/MainWindowController.java
@@ -11,31 +11,8 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.util.*;
 
-public class MainWindowController extends Application {
+public class MainWindowController  {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        primaryStage.setTitle("Profile Analyzer");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
 
-    public static void main(String[] args) {
-        String directory = "C:\\Users\\Tristan_Nibbe\\Downloads";
 
-        Parser parser = new Parser(directory);
-        Analyzer analyzer = new Analyzer();
-
-        File directoryPath = new File(directory + "\\Reference_Profiles");
-        String contents[] = directoryPath.list();
-
-        for(String file: contents) {
-            HashSet<String> parsedProfile = parser.parseProfile(file);
-
-            analyzer.analyzeProfile(parser.parseProfile(file));
-        }
-
-        analyzer.printCommonWords(3);
-    }
 }
