652c3eaeaaa6283d40090a79f9d9bdbe2af3fc39
Tristan Nibbe <tristan.nibbe114@myci.csuci.edu>
2021-05-23 19:40:20 -0700

    started wiring up ui to backend

--- src/ProfileAnalyzer/MainWindowController.java
+++ src/ProfileAnalyzer/MainWindowController.java
@@ -1,4 +1,22 @@
 package ProfileAnalyzer;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+
 public class MainWindowController {
+
+    @FXML
+    private MenuItem AnalyzeButton;
+
+    @FXML
+    private TextArea CommonWordsTextArea;
+
+    @FXML
+    void AnalyzeProfile(ActionEvent event) {
+        CommonWordsTextArea.appendText("adfsd");
+        System.out.println("ran");
+    }
+
 }
